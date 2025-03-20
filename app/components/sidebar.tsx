d13f2ba04cf889d1782f97323e55fbcd426c53b8
sunkimk <56373861+sunkimk@users.noreply.github.com>
2025-03-20 23:03:32 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,7 +250,7 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
+        title="ChatKK"
         subTitle="Build your own AI assistant."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
